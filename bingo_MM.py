0706325bc002f8ee02dcd8c577cfcc27b54209f5
KaylenTon <kaylent2k@gmail.com>
2026-07-21 14:38:56 -0400

    develop functions, edit comments, sample t/f statements

--- bingo_MM.py
+++ bingo_MM.py
@@ -1,3 +1,6 @@
+import pandas as pd
+import random
+
 # Bingo Card Arrangement
 # ------------------------------------
 # 00  01  02  03  04        B
@@ -8,33 +11,50 @@
 
 winning_patterns = [
 
-    # vertical
-    [0,1,2,3,4],                # B 
-    [5,6,7,8,9],                # I 
+    # horizontal
+    [0,1,2,3,4],           # B 
+    [5,6,7,8,9],           # I 
     [10,11,12,13,14],      # N 
     [15,16,17,18,19],      # G 
     [20,21,22,23,24],      # O 
 
     # diagonal
-    [0, 6, 12, 18, 24],      # \
-    [4, 8, 12, 16, 20],      #  / 
+    [0, 6, 12, 18, 24],    # \
+    [4, 8, 12, 16, 20],    #  / 
 
-    # horizontal
-    [0, 5, 10, 15, 20],      # B
-    [1, 6, 11, 16, 21],      # I
-    [2, 7, 12, 17, 22],      # N
-    [3, 8, 13, 18, 23],      # G
-    [4, 9, 14, 19, 24]       # O
+    # vertical
+    [0, 5, 10, 15, 20],    # B
+    [1, 6, 11, 16, 21],    # I
+    [2, 7, 12, 17, 22],    # N
+    [3, 8, 13, 18, 23],    # G
+    [4, 9, 14, 19, 24]     # O
 
 ]
 
-false_statements = list(range(1,50, 2))
-truth_statements = list(range(2,50, 2))
+false_statements = list(range(1,100, 2))
+truth_statements = list(range(2,100, 2))
 
-def create_set(winning_cards = 1):
-    total_cards = winning_cards * 20
-    losing_cards = total_cards - winning_cards
-    return(losing_cards, winning_cards)
+def create_split(total_winning_cards = 1):
+    total_cards = total_winning_cards * 20 # there is a winner for every twenty cards
+    losing_cards = total_cards - total_winning_cards
+    return(losing_cards, total_winning_cards)
 
-print(create_set(5))
-print(winning_patterns[1])
+winning_cards, losing_cards = create_split(total_winning_cards = 8)
+print(winning_cards)
+print(losing_cards)
+
+def create_winning_stack():
+
+    winning_stack = []
+
+    while len(winning_stack) < winning_cards:
+
+        select_random_pattern = random.choice(winning_patterns)
+        print(select_random_pattern)
+
+        card = ["Blank"] * 25
+        
+        card[12] = "FREE"
+
+        if card not in winning_stack:
+            winning_stack.append(card)
